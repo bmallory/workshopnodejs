6389e54130d302a5b70d6edbe0a835815612b5cb
cyril <bmallory@gmail.com>
2018-12-18 15:58:42 +0100

    bookstore + users

--- index.js
+++ index.js
@@ -3,53 +3,95 @@ var fs = require('fs');
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const bodyParser = require('body-parser')
+
+
+var cookieParser = require('cookie-parser')
+
+
+app.use(cookieParser())
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017');
 
 const Book = mongoose.model('Book', { name: String, id: String, orders_counter: Number });
+const UserBooks = mongoose.model('UserBooks', {name: String, pass : String});
 
-// const kitty = new Cat({ name: 'Zildjian' });
-// kitty.save().then(() => console.log('meow'));
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.get('/', function (req, res){
-    getAllProducts(function(err, products){
-        if(err){
-            console.log(err);
-            return
-        }
-
-    //    products.forEach(element =>{
-    //        book = new Book( {'name':element.name, 'id':element.id, 'orders_counter':element.orders_counter});
-    //        book.save();
-    //    })
 
         Book.find(function(err,books){
             res.render('index', {'products': books});
         })
         
-      })
-    
 });
 
 app.get('/order/:id', function(req,res){
-     Book.find(function(err,books){
-        products.forEach(
-            element => {
-                if(element.id == req.params.id){
-                    element.orders_counter++;
-                    console.log(`Commande terminée, Voici votre fichier : ${element.file_link}`);
-                }
-            }
-        )
 
-        fs.writeFile('./products.json', JSON.stringify(products), function(err){
-            if(err)console.log(err)
-        }  );
-        res.send('popo');
+    if(req.cookies.uname !== "" && req.cookies.uname !== null ){
+        UserBooks.findOne({'name':req.cookies.uname}, (err,user) => {
+
+            if(user){
+                Book.find({'id':req.params.id}, function(err,books){
+
+                    books.forEach(
+                        element => {
+                            if(element.id == req.params.id){
+                                element.orders_counter++;
+                                element.save();
+                                console.log(`Commande terminée, Voici votre fichier : ${element.file_link}`);
+                            }
+                        }
+                    )
+                    res.send('popo');
+                });
+
+                    
+            }else{
+           
+                res.send('nok');
+            }
+            
+        } );
+        
+    }
+
+    
+
+   
+});
+
+
+app.get('/signup', function(req, res){
+    res.render('signup');
+})
+
+app.post('/register', function(req, res){
+
+    user = new UserBooks({"name":req.body.user, "pass": req.body.pass});
+    user.save();
+    res.send('ok');
+
+})
+
+app.post('/login', function(req, res){
+    UserBooks.findOne({'name':req.body.user,'pass':req.body.pass}, (err,user) => {
+        if(user){
+            res.cookie('uname',req.body.user, { maxAge: 900000, httpOnly: true });
+            res.send('ok ! <a href="/">retour</a>');
+        }else{
+            req.send( "unamenok" )
+        }
     });
 });
 
@@ -57,22 +99,3 @@ app.get('/order/:id', function(req,res){
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
   })
-
-
-
-
-
-
-
-
-
-function getAllProducts( callback ){
-
-    fs.readFile('./products.json', 'utf8', (err,file) => {
-        products = JSON.parse(file);
-        callback(err, products);
-
-    });
-
-    
-}
